56a1cb38b0973298b309fff25196b7b39232c6e3
cecep31 <cecepjanuardi31@gmail.com>
2023-07-12 00:15:15 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
       ttl: 60,
       limit: 10,
     }),
-    UsersModule,
+    
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -41,6 +41,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
       ssl: true,
       cache: true,
     }),
+    UsersModule,
     AuthModule,
     TasksModule,
     PostsModule,
