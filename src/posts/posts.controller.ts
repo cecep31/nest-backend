94aa8dd8f884458d07d075692a1ec7ad6b1ada8f
cecep31 <cecepjanuardi31@gmail.com>
2023-09-26 12:00:53 +0700

    improve

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Get,
+  Param,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { posts as PostModel } from '@prisma/client'
@@ -9,10 +10,14 @@ import { posts as PostModel } from '@prisma/client'
 export class PostsController {
   constructor(private readonly postsService: PostsService) { }
 
-
   @Get()
   async findAll(): Promise<PostModel[]> {
     return this.postsService.posts({});
+  }
+
+  @Get(":id")
+  async post(@Param('id') id: string) {
+    return this.postsService.findById(id)
   }
 
 
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -26,10 +26,14 @@ export class PostsService {
     });
   }
 
+  findById(id: string) {
+    return this.prisma.posts.findUnique({ where: { id: id } })
+  }
 
   commentCreate(data: any) {
     return this.prisma.post_comments.create({ data: data });
   }
+
   getAllcomment(post_id: string): Promise<post_comments[]> {
     return this.prisma.post_comments.findMany({
       where: { post_id: post_id, parrent_comment_id: null },
