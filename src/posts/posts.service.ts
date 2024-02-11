7a91184a7e15dffb062866f7a588efe18bd60e56
cecep31 <cecepjanuardi31@gmail.com>
2024-02-11 12:30:13 +0700

    update

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -32,13 +32,14 @@ export class PostsService {
 
 
   commentCreate(data: any) {
+    data.created_at = new Date();
     return this.prisma.post_comments.create({ data: data });
   }
 
   getAllcomment(post_id: string): Promise<post_comments[]> {
     return this.prisma.post_comments.findMany({
       where: { post_id: post_id, parrent_comment_id: null },
-      orderBy: { created_at: "desc" }
+      orderBy: { created_at: 'desc' }
     });
   }
 
