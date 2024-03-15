5ed3afd05b0282c54ab2a630a714c1065f274b86
cecep31 <cecepjanuardi31@gmail.com>
2024-03-15 22:49:35 +0700

    fix some logger for pino

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { bufferLogs: true });
   app.enableCors({ origin: "*" })
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
