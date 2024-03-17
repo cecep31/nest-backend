65ca7fa110994840789a68521d8e2d0f64df50c7
cecep31 <cecepjanuardi31@gmail.com>
2024-03-17 22:24:34 +0700

    update package

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaService } from './prisma.service';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 30,
-    }),
     LoggerModule.forRoot(
       {
         pinoHttp: {
