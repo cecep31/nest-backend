3929025c341aa08d3add0926942e3ec83731fcfe
cecep <cecepjanuardi@outlook.com>
2025-05-29 12:54:38 +0700

    fix again

--- src/modules/tags/tags.module.ts
+++ src/modules/tags/tags.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
-import { PrismaService } from 'src/db/prisma.service';
+import { PrismaService } from '../../db/prisma.service';
 
 @Module({
   controllers: [TagsController],
